dea23b8f3c4ae13c4a85229129fb21661c44119e
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-02-05 12:53:10 -0700

    finish blog comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Blogger::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end

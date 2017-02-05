c18ba737d1a90a824fcb06fece4aed2ab3bb554b
Courtney Meyerhofer <meyerhoferc@gmail.com>
2017-02-05 16:58:17 -0700

    tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,18 @@
+class TagsController < ApplicationController
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash.notice = "Tag '#{@tag.name}' was deleted!"
+
+    redirect_to article_path(@articles)
+  end
+end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def article_params
+    params.reuire(:article).permit(:title, :body, :tag_list)
+  end
 end
